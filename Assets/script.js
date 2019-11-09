4a2d23977517f9d259264911067c5191b0f8ce39
JGen1 <jengenes01@gmail.com>
2019-11-09 01:37:16 -0500

    Added highscores file, created stopwatch function, added highscore link.

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,26 @@
 //
 
-//for(var i =0; i < questions.length; i++){
- //   response += questions[i];
- //   if(response === questions[i].answer){
- //       score++
- //   } else{
- //       timer--;
- //   }
-//}
+var score = 0;
 
 
+for(var i =0; i < questions.length; i++){
+    var response = document.getElementById("quiz").innerHTML;
+    if(response =+ questions[i].answer){
+        score++
+    } else{
+        timer++;
+    }
+}
+//Stopwatch
+var myTimer;
+function clock() {
+    myTimer = setInterval(myClock, 1000);
+    var c = 0;
+
+    function myClock() {
+        document.getElementById("timer").innerHTML = ++c;
+        if (c == 0) {
+        clearInterval(myTimer);
+       }
+    }
+}
