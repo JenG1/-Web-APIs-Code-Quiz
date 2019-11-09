b02a1336f6b272d072f68d86570da67daff2510c
JGen1 <jengenes01@gmail.com>
2019-11-08 22:34:30 -0500

    Created folders assets, images. Added quiz buttons, timer design. Created questions js file, added image file, added css file.

---
+++ Assets/questions.js
@@ -0,0 +1,52 @@
+
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "a"
+  },
+  {
+    question: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "b"
+  },
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "b"
+  },
+  {
+    question: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "d"
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "d"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "a"
+  },  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "b"
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "a"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "a"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "a"
+  }
+];
---
+++ Assets/script.js
@@ -0,0 +1,12 @@
+//
+
+//for(var i =0; i < questions.length; i++){
+ //   response += questions[i];
+ //   if(response === questions[i].answer){
+ //       score++
+ //   } else{
+ //       timer--;
+ //   }
+//}
+
+
