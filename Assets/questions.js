cdfd219453e39b65506dc2bb128694f346245c61
JGen1 <jengenes01@gmail.com>
2019-11-09 17:27:25 -0500

    CSS style for highscores page, reorganize questions object and defined each user option item.

--- Assets/questions.js
+++ Assets/questions.js
@@ -2,51 +2,81 @@
 var questions = [
   {
     question: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "a"
+    choices: "A",
+    choices: "B",
+    choices: "C",
+    choices: "D",
+    answer: "1"
   },
   {
     question: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "b"
+    choices: "A",
+    choices: "B",
+    choices: "C",
+    choices: "D",
+    answer: "3"
   },
   {
     question: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "b"
+    choices: "A",
+    choices: "B",
+    choices: "C",
+    choices: "D",
+    answer: "2"
   },
   {
     question: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "d"
+    choices: "A",
+    choices: "B",
+    choices: "C",
+    choices: "D",
+    answer: "1"
   },
   {
     title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "d"
+    choices: "A",
+    choices: "B",
+    choices: "C",
+    choices: "D",
+    answer: "1"
   },
   {
     title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "a"
+    choices: "A",
+    choices: "B",
+    choices: "C",
+    choices: "D",
+    answer: "1"
   },  {
     title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "b"
+    choices: "A",
+    choices: "B",
+    choices: "C",
+    choices: "D",
+    answer: "3"
   },
   {
     title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "a"
+    choices: "A",
+    choices: "B",
+    choices: "C",
+    choices: "D",
+    answer: "2"
   },
   {
     title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "a"
+    choices: "A",
+    choices: "B",
+    choices: "C",
+    choices: "D",
+    answer: "4"
   },
   {
     title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "a"
+    choices: "A",
+    choices: "B",
+    choices: "C",
+    choices: "D",
+    answer: "3"
   }
 ];
